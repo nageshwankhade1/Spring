edff1959b28fb752ff20f48442e6eb876e52c4fa
Nagesh <nageshwankhade1@gmail.com>
2021-04-28 20:28:50 +0530

    Initial Commit

---
+++ manual-di/src/main/java/nmw/springframework/manualdi/ManualDiApplication.java
@@ -0,0 +1,13 @@
+package nmw.springframework.manualdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualDiApplication.class, args);
+	}
+
+}
---
+++ manual-di/src/test/java/nmw/springframework/manualdi/ManualDiApplicationTests.java
@@ -0,0 +1,13 @@
+package nmw.springframework.manualdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
